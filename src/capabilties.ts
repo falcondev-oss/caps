7d0c109b23bc306a606aa6bc6e902842d783d825
Julian Meinking <12785972+DrJume@users.noreply.github.com>
2025-01-21 13:49:16 +0100

    feat: add mode type helper

--- src/capabilties.ts
+++ src/capabilties.ts
@@ -1,4 +1,13 @@
-import type { Exact, IfEmptyObject } from 'type-fest'
+import type { Exact, IfEmptyObject, TaggedUnion } from 'type-fest'
+
+export type Modes<T extends Record<string, Record<string, unknown>>> = TaggedUnion<'__mode', T>
+
+export function mode<const K extends string, T extends Record<string, unknown>>(
+  key: K,
+  obj: T,
+): { __mode: K } & T {
+  return { __mode: key, ...obj }
+}
 
 export function arg<T extends object>() {
   return {} as T
